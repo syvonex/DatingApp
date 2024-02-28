70d9384bd2a4b3d0f3edfcde3d71a81be5b0d208
huichunng <bryanng5479@gmail.com>
2024-02-28 11:24:50 +0800

    upload

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Carmen");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Bryan");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Ahtai");
---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<API.Data.DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
